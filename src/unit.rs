0275f47a0a3d7c84f69bd9e9deb8cb903c41571a
Caleb Dastrup <calebjdastrup@gmail.com>
2023-10-24 17:11:48 -0600

    unit subset

--- src/unit.rs
+++ src/unit.rs
@@ -1,6 +1,6 @@
 use std::marker::PhantomData;
 
-use crate::{structure::{ring::{Ring, RingOperations}, group::Group, monoid::Monoid}, operation::O2, set::Set};
+use crate::{structure::{ring::{Ring, RingOperations}, group::Group, monoid::Monoid, euclidean_ring::EuclideanRing}, operation::O2, set::{Set, Subset}};
 
 pub struct Unit<R:Ring<O>,O:RingOperations<R>> {
     u: R,
@@ -34,5 +34,31 @@ impl<R:Ring<O>,O:RingOperations<R>> Monoid<O::TIMES> for Unit<R,O> {
 impl<R:Ring<O>,O:RingOperations<R>> Group<O::TIMES> for Unit<R,O> {
     fn inverse(self) -> Self {
         Unit { u: self.u_inverse, u_inverse: self.u, o: PhantomData }
+    }
+}
+
+pub trait TryInverse<O:RingOperations<Self>>: Ring<O> {
+    //This must return the multiplicative inverse of self, or None if no such inverse exists.
+    fn try_inverse(self)->Option<Self>;
+}
+impl<R:EuclideanRing<O>,O:RingOperations<R>> TryInverse<O> for R {
+    fn try_inverse(self)->Option<Self> {
+        let q=R::one().divide(self.clone());
+        if R::one()==q.clone().times(self) {
+            Some(q)
+        } else {
+            None
+        }
+    }
+}
+impl<R:Ring<O>,O:RingOperations<R>> Subset<R> for Unit<R,O> where R:TryInverse<O> {
+    fn contains(t: &R) -> bool {
+        t.clone().try_inverse().is_some()
+    }
+    fn inclusion(self) -> R {
+        self.u
+    }
+    fn try_from(t: R) -> Self {
+        Unit { u: t.clone(), u_inverse: t.try_inverse().unwrap(),o:PhantomData}
     }
 }
